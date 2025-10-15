091cc5f33782df1c5797a993fadf162070c205b5
MZhafran <muhd.zhafran@gmail.com>
2025-10-15 17:06:02 +0800

    created testapp.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './TestApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h2>hello</h2>
     <App />
   </StrictMode>,
 )
