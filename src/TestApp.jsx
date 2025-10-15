01212a2852060b05f859c2e78c63987e5a2367a8
MZhafran <muhd.zhafran@gmail.com>
2025-10-15 17:09:34 +0800

    update test app

--- src/TestApp.jsx
+++ src/TestApp.jsx
@@ -1,7 +1,9 @@
+import './App.css'
+
 export default function App() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-yellow-100">
-      <h1 className="text-3xl font-bold text-black">Hello, Zhafran 🚀</h1>
+      <h1 className="text-3xl font-bold text-black font-mono">Hello, Zhafran 🚀</h1>
     </main>
   );
 }
