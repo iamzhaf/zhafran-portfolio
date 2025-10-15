17ab4dc7273d382d7d9d561617f8ea27ffbd7649
MZhafran <muhd.zhafran@gmail.com>
2025-10-15 17:26:50 +0800

    updated main.jsx

--- src/TestApp.jsx
+++ src/TestApp.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 
-export default function App() {
+export default function TestApp() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-yellow-100">
+    <main className="min-h-screen flex items-center justify-center bg-yellow-500">
       <h1 className="text-3xl font-bold text-black font-mono">Hello, Zhafran 🚀</h1>
     </main>
   );
